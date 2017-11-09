9e7a38ddaa1090aef50b3ce6b8d8b9f1fe3df429
Robert Kalescky <rkalescky@smu.edu>
2017-11-09 13:19:54 -0600

    Added separate text file for getting OAuth information.

--- 1_unigram_1000.R
+++ 1_unigram_1000.R
@@ -4,12 +4,12 @@ library(magrittr)
 library(tm)
 library(ggplot2)
 
+# Get Twitter OAuth data
+oauth <- scan("twitter_oauth.txt", character(), quote = "")
+
 # Setup Twitter application access using OAuth
 options(httr_oauth_cache=T)
-setup_twitter_oauth("nkywhHqejlG9RlvEX19WWSnPs",
-"WBwKTRWp1OzL3Q9tAZXUcC1QX1lfqzYxXKe6PFr1S0wsJZEUri",
-"927642335864815616-nNKPHs4rRlxeXseQYD5HSroLEhOdIZ6",
-"YoUbIvZ26fhYqnfydJakgI2jj7fPwk6tRzuXmoJ1i4L2p")
+setup_twitter_oauth(oauth[1], oauth[2], oauth[3], oauth[4])
 
 # Search for tweets
 tweets <- searchTwitter('#metoo', n = 1000, lang = 'en')
